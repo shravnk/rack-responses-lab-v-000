c5e3b1014d4fac6c74777b2f1fc85c0d5fb1aaa9
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-19 23:23:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,19 @@
 require 'date'
-require 'pry'
+
 
 class Application
 
   def call(env)
     resp = Rack::Response.new
-    time =
-    binding.pry
+    t = Time.new
+
+    if t.hour >= 12 
+      resp.write "Good Morning!"
+    elsif t.hour <= 12
+      resp.write "Good Afternoon!"
+    end    
+    
+
   end
 
 end
